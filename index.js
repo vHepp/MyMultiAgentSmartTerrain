49c7d540320f2d6491c91732b57950ab874cbbee
Vincent Hepola <63828592+vHepp@users.noreply.github.com>
2022-12-16 22:40:26 -0500

    working prototype

--- index.js
+++ index.js
@@ -16,14 +16,55 @@ class Solver {
 			for (let i = 0; i < percents.length; i++) {
 				let sacrifice = 0;
 				for (let k = 1; k < percents.length; k++) {
-					sacrifice += percents[(i + k) % percents.length];
+					sacrifice += percents[(i + k) % percents.length]
+					//sacrifice = Math.max(sacrifice, percents[(i + k) % percents.length])
 				}
 				let gain = percents[i];
 
-				//Math.round(((sacrifice / gain) + Number.EPSILON) * 100) / 100
-				//let oppCost = `${sacrifice / gain}`
-				let oppCost = Math.round(((sacrifice / gain) + Number.EPSILON) * 1000) / 1000
+				//l`et oppCost = `${sacrifice} / ${gain}`
+				let oppCost = Math.round(((sacrifice / gain) + Number.EPSILON) * 100) / 100
 				this.costs[g][i] = oppCost;
+			}
+		}
+
+	}
+
+	optimize() {
+
+		console.log("OPTIMIZE")
+		console.log(this.costs.length)
+		console.log(this.costs[0].length)
+
+
+		this.min = 0;
+		this.minArr = [];
+		if (this.costs && this.costs[0]) {
+			this.min = this.costs[0][0];
+			for (let i = 0; i < this.costs[0].length; i++) {
+				this.minArr.push(0);
+			}
+		}
+		else { return }
+
+		/* console.log(`min = ${this.min}`);
+		console.log("minArr:")
+		console.table(this.minArr) */
+
+		//for 3x3 options
+		for (let i = 0; i < this.costs[0].length; i++) {
+			for (let j = 0; j < this.costs[1].length; j++) {
+				for (let k = 0; k < this.costs[2].length; k++) {
+					if (i !== j && i !== k && j !== k) {
+						let cost = this.costs[0][i] + this.costs[1][j] + this.costs[2][k];
+
+						if (cost < this.min) {
+							this.min = cost;
+							this.minArr[0] = i;
+							this.minArr[1] = j;
+							this.minArr[2] = k;
+						}
+					}
+				}
 			}
 		}
 
@@ -36,25 +77,63 @@ class Solver {
 	displayCosts() {
 		console.table(this.costs);
 	}
+
+	displayChoices() {
+		let result = {
+			totalCost: this.min,
+			choices: this.minArr
+		}
+		console.table(result);
+	}
 }
 // enddefine
 
 
 const guards = [{
 	guard: "0",
-	percents: [35, 50]
+	percents: [10, 50, 95]
 },
 {
 	guard: "1",
-	percents: [30, 55]
+	percents: [10, 50, 40]
 },
 {
 	guard: "2",
-	percents: [50, 55]
+	percents: [10, 85, 95]
 },
 ];
 
 const s = new Solver(guards);
 
+s.optimize();
 s.displayGuards()
 s.displayCosts()
+s.displayChoices()
+/* 
+let combos = 0;
+
+for (let i = 0; i < 3; i++) {
+	for (let j = 0; j < 3; j++) {
+		for (let k = 0; k < 3; k++) {
+			if (i !== j && i !== k && j !== k) {
+				combos++;
+			}
+		}
+	}
+}
+console.log(`unique selections 3x3 combos: ${combos}`)
+
+combos = 0;
+for (let i = 0; i < 3; i++) {
+	for (let j = 0; j < 3; j++) {
+		for (let k = 0; k < 3; k++) {
+			//console.table({ i, j, k })
+			if (i !== j && i !== k && j !== k) {
+				combos++;
+			}
+		}
+	}
+}
+
+console.log(`unique v2 selections 3x3 combos: ${combos}`)
+*/
